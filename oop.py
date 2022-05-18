2a14a4a0af61a6df0635e2e6c1197ae54aaddc8d
SilentKush <osvaldsilent@gmail.com>
2022-05-18 13:35:51 -0500

    Calculator method added to OOP example

--- oop.py
+++ oop.py
@@ -6,6 +6,19 @@ class user:
         self.sex="M"
         self.age=25
 
+    def __int__(self,n1,n2):
+        self.suma=n1+n2
+        self.resta=n1-n2
+        self.division=n1/n2
+        self.multiplication=n1*n2
+
+
+
 u = user()
 u.default()
+u.__int__(10,2)
 print(f"The name of the user is {u.name} and is {u.age} years old");
+print(u.suma)
+print(u.resta)
+print(u.division)
+print(u.multiplication)

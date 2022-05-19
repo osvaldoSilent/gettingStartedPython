0cf63484d35b61841e70875305dc7115bb3b37a0
SilentKush <osvaldsilent@gmail.com>
2022-05-18 22:11:28 -0500

    menus and loops added to OOP example

--- oop.py
+++ oop.py
@@ -11,24 +11,46 @@ class user:
         self.resta=n1-n2
         self.division=n1/n2
         self.multiplication=n1*n2
-        self.launchMenu=int(input(f"The name of the user is {u.name} and is {u.age} years old \n"
-                                        f"What you wanna do?\n"
+
+    def launchCalculatorMenu(self):
+        return int(input(f"The name of the user is {getattr(u,'name')} and is {u.age} years old \n"
+                                        f"Chose an option?\n"
                                         f"1-Sumar\n"
                                         f"2-Restar\n"
-                                        f"2-dividir\n"
-                                        f"2-multiplicar\n"))
-
+                                        f"3-dividir\n"
+                                        f"4-multiplicar\n"))
+    def launchGameMenu(self):
+        return int(input(f"Welcome to my APP, what you wanna do? \n"
+                         f"What you wanna do?\n"
+                         f"1-Set new user\n"
+                         f"2-Calculator\n"
+                         f"3-Exit\n"))
 
 u = user()
 u.default()
-u.__int__(10,2)
+u.__int__(10, 2)
+optionSelected=0
 
-option = u.launchMenu
-if(option==1):
-    print(u.suma)
-elif(option==2):
-    print(u.resta)
-elif(option==3):
-    print(u.division)
-elif(option==4):
-    print(u.multiplication)
+while(optionSelected!=3):
+    optionSelected = u.launchGameMenu()
+
+    if(optionSelected == 1):
+        newName = input("insert name for new user: ")
+        newAge = input("insert age for new user: ")
+        newSex = input("insert sex for new user: ")
+        setattr(u,'name',newName)
+        setattr(u,'age',newAge)
+        setattr(u,'sex',newSex)
+
+    elif(optionSelected == 2):
+        optionCalculator=0;
+        while(optionCalculator!=5):
+            optionCalculator=u.launchCalculatorMenu()
+            if(optionCalculator==1):
+                print(u.suma+"\n\n\n\n\n")
+            elif(optionCalculator==2):
+                print(u.resta+"\n\n\n\n\n")
+            elif(optionCalculator==3):
+                print(u.division+"\n\n\n\n\n")
+            elif(optionCalculator==4):
+                print(u.multiplication+"\n\n\n\n\n")

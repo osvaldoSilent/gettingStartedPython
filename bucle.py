fa9904becc354cb3b6af63e9a8cd1a6cca3ce337
SilentKush <osvaldsilent@gmail.com>
2022-06-06 23:26:25 -0500

    tricks for bucles

---
+++ bucle.py
@@ -0,0 +1,14 @@
+count = 0;
+while (True):
+    count +=1
+    if(count==2):
+        print("continuing")
+        continue
+
+    print("Este mensaje se excluye")
+
+    print(count)
+    if(count>5):
+        pass
+    if(count==10):
+        break;

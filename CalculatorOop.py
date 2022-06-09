d189d21221cf269c57ca8a2765dfabf7e65b5509
SilentKush <osvaldsilent@gmail.com>
2022-06-09 10:44:41 -0500

    learning about packages and modules

--- CalculatorOop.py
+++ CalculatorOop.py
@@ -1,29 +1,38 @@
 class Calculator:
+    pass
     def __int__(self,n):
-        self.n=n
         self.data=[0 for i in range(n)]
-    def setData(self):
-        self.data=[int(input("insert number")) for i in range(self.n)]
+    def setData(self,n):
+        self.data=[int(input("insert number")) for i in range(n)]
 class BasicCalculator (Calculator):
-    def __int__(self,n):
-        Calculator.__int__(self,2)
+    pass
+    def __int__(self):
+        super().__int__(2)
     def suma(self):
-        Calculator.setData(self)
+        super().setData(2)
         a,b,=self.data
         return print(a+b)
 import math;
 class AdvancedCalculator (Calculator):
-    def __int__(self,n):
-        Calculator.__int__(self,1)
+    pass
+    def __int__(self):
+        super().__int__(1)
     def raiz(self):
-        Calculator.setData(self)
+        super().setData(1)
         a,=self.data
         return print(int(math.sqrt(a)))
+class Full(AdvancedCalculator, BasicCalculator):
+    pass
+    def __int__(self):
+        pass
 
-basicCalculator = BasicCalculator();
-basicCalculator.__int__(2)
+
+
+
+#basicCalculator = BasicCalculator();
+#basicCalculator.__int__(2)
 #basicCalculator.suma()
 
-advancedCalculator = AdvancedCalculator();
-advancedCalculator.__int__(1)
-advancedCalculator.raiz()
+#advancedCalculator = AdvancedCalculator();
+#advancedCalculator.__int__(1)
+#advancedCalculator.raiz()
---
+++ main.py
@@ -0,0 +1,8 @@
+import CalculatorOop
+
+
+full = CalculatorOop.Full()
+full.__int__()
+full.suma()
+full.raiz()
+
